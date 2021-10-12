0d457b210ddfe3c0008946a45eb563a2cbc60895
OteroSantiago <91058589+OteroSantiago@users.noreply.github.com>
2021-10-12 20:51:50 +0200

    Initial commit

---
+++ src/tiposflotantes/TiposFlotantes.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiposflotantes;
+
+/**
+ *
+ * @author Santiago
+ */
+public class TiposFlotantes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Tipos Flotantes con decimales
+        //Tipo Float
+        float numeroFloat = 10.0F;
+        System.out.println(numeroFloat);
+        System.out.println("Valor mínimo del tipo float: "+Float.MIN_VALUE);
+        System.out.println("Valor máximo del tipo floar: "+Float.MAX_VALUE);
+        //O
+        double numeroFloat2 = 10.0;
+        System.out.println(numeroFloat2);
+        System.out.println("Valor mínimo del tipo float(2): "+Float.MIN_VALUE);
+        System.out.println("Valor máximo del tipo float(2): "+Float.MAX_VALUE);
+        //Tipo Double
+        //double numerDouble = 10.0 // este es de tipo double
+        double numeroDouble2 = 10;
+        System.out.println(numeroDouble2);
+        System.out.println("Valor mínimo del tipo double: "+Double.MIN_VALUE);
+        System.out.println("Valor máximo del tipo double: "+Double.MAX_VALUE);
+        
+       
+    }
+    
+}
